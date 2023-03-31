b968bd59ad4e1267bc4d86d503cb29b5ecdfbd13
Ali Bridgers <abridgers@gmail.com>
2023-03-30 21:09:32 -0500

    quick change to test

---
+++ Alis/quick_check_up.py
@@ -0,0 +1,15 @@
+# Print Hello User!
+print("Hello user!")
+
+# Take in User Input
+secret = str(input("Tell me a secret. I won't tell it to anyone else... "))
+
+# Respond Back with User Input
+print(f'Except I am a sneaky python and will tell everyone that your secret is {secret}')
+
+# Take in the User Favorite Number
+fave_num = int(input("Now, tell me your favorite number... "))
+
+# Respond Back with a statement based on your favorite number
+my_fave_num = 87
+print(f'"Your favorite number is {fave_num}, but my favorite number is {my_fave_num}"')
