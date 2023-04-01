03aeedb6efb730f587e2478592c0472fb54998cb
LeAwesomeGuy <adam.n.kertz@gmail.com>
2023-03-31 22:34:39 -0500

    made a small update to the main.py file in the Adam folder

--- Adam/main.py
+++ Adam/main.py
@@ -1,1 +1,7 @@
-print("Hi Tom! It's Adam")
+name = "Tom"
+condition = "y"
+while condition == "y":
+    print(f"Hi {name}! It's Adam")
+    condition = input("Would you like to see another greeting? (type 'y' for yes, type 'n' for no) ")
+    if condition == "y":
+        name = input("Who am I sending this greeting to? (type in their name) ")
